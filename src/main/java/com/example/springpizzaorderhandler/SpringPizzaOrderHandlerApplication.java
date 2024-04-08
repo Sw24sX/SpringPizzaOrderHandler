e913b7971b442d35edddc9d0f2a17e5a763ed53d
sw24sx <aleksandr.korolyov.99@mail.ru>
2024-04-08 16:09:03 +0500

    Init commit

---
+++ src/main/java/com/example/springpizzaorderhandler/SpringPizzaOrderHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpizzaorderhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPizzaOrderHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPizzaOrderHandlerApplication.class, args);
+    }
+
+}
